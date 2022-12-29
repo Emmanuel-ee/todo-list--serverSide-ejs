0f652ae07d6ad28b48fb5b20a461d4696d2aecab
Emmanuel <ememeemmanuel3@gmail.com>
2022-12-29 00:02:54 +0000

    css added with public file and express static

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 let tasks = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.get("/", function (req, res) {
